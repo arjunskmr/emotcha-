2a80bbfc5dd16db68b6e2973db65815ffef8ef44
arjun <arjunskmr@gmail.com>
2015-02-09 01:31:17 +0530

    testing database

--- server.php
+++ server.php
@@ -13,12 +13,19 @@ if ($conn->connect_error) {
     
 } 
 echo "Connected successfully";
-$sql = "SELECT * FROM session;";
+$sql = "SELECT * FROM active_table";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
    
-        echo $row["sl_no"];
+        echo $row["session_id"];
+        echo "<br>":
+        echo $row["image_id"];
+        echo "<br>":
+        echo $row["emotion_name"];
+        echo "<br>":
+        echo $row["image_name"];
+        echo "<br>":
     
 } else {
     echo "0 results";
