42d4ac211f5f77b13f646b129c3266bdc5c3f86b
arjun <arjunskmr@gmail.com>
2015-02-18 09:49:02 +0530

    json encoded

--- server.php
+++ server.php
@@ -27,11 +27,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     
 } 
-echo "<br>". $_GET['apikey']. "<br />";
-echo "-- ". $_GET['apisecret']. " --<br>";
-echo "Connected successfully";
-echo "$apikey";
-echo "$apisecret";
+// echo "<br>". $_GET['apikey']. "<br />";
+// echo "-- ". $_GET['apisecret']. " --<br>";
+// echo "Connected successfully";
+// echo "$apikey";
+// echo "$apisecret";
 mysql_select_db('emotcha_db');
 $val = mysql_query('select * from `image_table` ORDER BY RAND() LIMIT 2');
 
@@ -45,7 +45,7 @@ while($row = mysql_fetch_array($val, MYSQL_ASSOC))
     $r[]=$row;
     print json_encode($r);
 } 
-echo "Fetched data successfully\n";
+// echo "Fetched data successfully\n";
 
 
 
