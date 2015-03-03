53d3ff7dbc3359c3c20d1b2770ee788a68b3e92d
arjunskmr <arjunskmr@gmail.com>
2015-03-03 11:09:27 +0530

    json_fix

--- server.php
+++ server.php
@@ -27,24 +27,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     
 } 
-echo "<br>". $_GET['apikey']. "<br />";
-echo "-- ". $_GET['apisecret']. " --<br>";
-echo "Connected successfully";
-echo "$apikey";
-echo "$apisecret";
+// echo "<br>". $_GET['apikey']. "<br />";
+// echo "-- ". $_GET['apisecret']. " --<br>";
+// echo "Connected successfully";
+// echo "$apikey";
+// echo "$apisecret";
 mysql_select_db('emotcha_db');
 $val = mysql_query('select * from `image_table` ORDER BY RAND() LIMIT 2');
 
 while($row = mysql_fetch_array($val, MYSQL_ASSOC))
 {
-    echo "{$row['image_id']}  <br> ".
-         " {$row['image_link']} <br> ".
-         " {$row['emotion_id']} <br> ".
-         " {$row['emotion_name']} <br> ".
-         "--------------------------------<br>";
+    // echo "{$row['image_id']}  <br> ".
+    //      " {$row['image_link']} <br> ".
+    //      " {$row['emotion_id']} <br> ".
+    //      " {$row['emotion_name']} <br> ".
+    //      "--------------------------------<br>";
+    $r[]=$row;
+    
 } 
-echo "Fetched data successfully\n";
-
+// echo "Fetched data successfully\n";
+print json_encode($r[]);
 
 
 $conn->close();
