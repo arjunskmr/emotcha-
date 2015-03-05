44579354975e9ae76b3e1206bfd8c8f25d9de5c6
arjunskmr <arjunskmr@gmail.com>
2015-03-05 14:05:17 +0530

    json fix1

--- server.php
+++ server.php
@@ -52,11 +52,22 @@ while($row = mysql_fetch_array($val, MYSQL_ASSOC))
 $emo_name =$r[0]["emotion_name"];
 $emo_id = $r[0]["emotion_id"];
 $image_id = $r[0]["image_id"];
+$image_link = $r[0]["image_link"]
+$emo_name1 =$r[1]["emotion_name"];
+$emo_id1 = $r[1]["emotion_id"];
+$image_id1 = $r[1]["image_id"];
+$image_link1 = $r[1]["image_link"]
 
 echo json_encode(array(
     array('name' => 'emo_name', 'data' => $emo_name),
     array('name' => 'emo_id', 'data' => $emo_id),
-    array('name' => 'image_id', 'data' => $image_id)
+    array('name' => 'image_id', 'data' => $image_id),
+    array('name' => 'image_link', 'data' => $image_link),
+    array('name' => 'emo_name', 'data' => $emo_name1),
+    array('name' => 'emo_id', 'data' => $emo_id1),
+    array('name' => 'image_id', 'data' => $image_id1),
+    array('name' => 'image_link', 'data' => $image_link1),
+
 ));
 
 
