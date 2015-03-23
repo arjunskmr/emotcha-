8ebc6bddcd4baf5b554c5a764f553e4c5df453b6
ARJUN S KUMAR <arjunskmr@gmail.com>
2015-03-24 02:48:12 +0530

    Update json_handle.php

--- json_handle.php
+++ json_handle.php
@@ -1,8 +1,43 @@
 <?php
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+$servername = "localhost";
+$username = "root";
+$password = "limboda";
+$conn = mysqli_connect ($servername, $username, $password, "emotcha_db");
+$val = mysqli_query($conn, 'select * from `active_table`');
+while($row = mysqli_fetch_array($val, MYSQL_ASSOC))
+{
+    $r[]=$row;
+    
+} 
+$count=0;
+  
     $posted_data = array();
     if (!empty($_POST['json'])) {
         $posted_data = json_decode($_POST['json'], true);
     }
-    print_r($posted_data);
-    print_r("hello");
+    if($r[0]["image_id"]==$posted_data["id1"]&&$r[0]["emotion_id"]!=$posted_data["txt1"])
+    {
+        $count++;
+    }
+     if($r[1]["image_id"]==$posted_data["id2"]&&$r[1]["emotion_id"]!=$posted_data["txt2"])
+    {
+        $count++;
+    }
+     if($r[2]["image_id"]==$posted_data["id3"]&&$r[2]["emotion_id"]!=$posted_data["txt3"])
+    {
+        $count++;
+    }
+     if($r[3]["image_id"]==$posted_data["id4"]&&$r[3]["emotion_id"]!=$posted_data["txt4"])
+    {
+        $count++;
+    }
+    
+    if($count==0)
+    {print_r("success");}
+    
+    if($count!=0)
+    {print_r("faliure");}
+    
 ?>
