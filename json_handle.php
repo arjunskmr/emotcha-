a55d6c3c5003cd4e25d5f8e9eb1a707119aa06ab
ARJUN S KUMAR <arjunskmr@gmail.com>
2015-03-24 03:21:47 +0530

    Update json_handle.php

--- json_handle.php
+++ json_handle.php
@@ -16,6 +16,8 @@ $count=0;
     $posted_data = array();
     if (!empty($_POST['json'])) {
         $posted_data = json_decode($_POST['json'], true);
+          echo $posted_data;
+          
         echo $r[0]["image_id"];
     echo $posted_data["id1"];
     echo $r[0]["emotion_id"];
