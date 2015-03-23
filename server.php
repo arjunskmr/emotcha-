1b2395349e2c29fd92f1f47a82bae9dfa2a787d4
ARJUN S KUMAR <arjunskmr@gmail.com>
2015-03-24 02:17:18 +0530

    Update server.php

--- server.php
+++ server.php
@@ -47,7 +47,10 @@ while($row = mysqli_fetch_array($val, MYSQL_ASSOC))
 $my_array = array(0,1,2,3);
 
 shuffle($my_array);
-
+$e = $r[0]["emotion_name"];
+$f = $r[1]["emotion_name"];
+$g = $r[2]["emotion_name"];
+$h = $r[3]["emotion_name"];
 //echo "Fetched data successfully\n";
 
 //print json_encode($r[0]);
@@ -67,10 +70,10 @@ $emo_name3 =$r[$my_array[3]]["emotion_name"];
 $emo_id3 = $r[3]["emotion_id"];
 $image_id3 = $r[3]["image_id"];
 $image_link3 = $r[3]["image_link"];
-mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id','$r[0]['emotion_name']');");
-mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id1','$r[1]['emotion_name']');");
-mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id2','$r[2]['emotion_name]');");
-mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id3','$r[3]['emotion_name']');");
+mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id','$e');");
+mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id1','$f');");
+mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id2','$g');");
+mysqli_query($conn, "INSERT INTO active_table VALUES ('session101','$image_id3','$h');");
 echo json_encode(array('emo_name' => $emo_name , 'emo_id' => $emo_id , 'image_id' => $image_id , 'image_link' => $image_link , 'emo_name1' => $emo_name1 , 'emo_id1' => $emo_id1 , 'image_id1' => $image_id1 , 'image_link1' => $image_link1 , 'emo_name2' => $emo_name2 , 'emo_id2' => $emo_id2 , 'image_id2' => $image_id2 , 'image_link2' => $image_link2 ,'emo_name3' => $emo_name3 , 'emo_id3' => $emo_id3 , 'image_id3' => $image_id3 , 'image_link3' => $image_link3));
 
 
