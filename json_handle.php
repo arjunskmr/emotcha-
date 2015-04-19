df8b213963bf486674c2d17ccf98cc8fc6ac1719
System Administrator <root@Arjuns-MacBook-Pro.local>
2015-04-19 23:51:22 +0530

    aad

--- json_handle.php
+++ json_handle.php
@@ -9,12 +9,12 @@ $val = mysqli_query($conn, 'select * from `active_table`');
 while($row = mysqli_fetch_array($val, MYSQL_ASSOC))
 {
     $r[]=$row;
-   
+    
 } 
 $count=0;
   
     $posted_data = array();
-  
+    
 
     if (!empty($_POST['json'])) {
         $posted_data = json_decode($_POST['json'], true);
