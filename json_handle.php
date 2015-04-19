2c76c42d537a721b1825c86292284705ad44bd9a
System Administrator <arjunskmr@gmail.com>
2015-04-20 02:27:55 +0530

    asd

--- json_handle.php
+++ json_handle.php
@@ -18,23 +18,11 @@ $count=0;
         $posted_data = json_decode($_POST['json'], true);
        
     }
-    if($r[0]["image_id"]==$posted_data["id1"]&&$r[0]["emotion_id"]!=$posted_data["txt1"])
+    if(($r[0]["image_id"]==$posted_data["id1"]&&$r[0]["emotion_id"]!=$posted_data["txt1"])||(($r[1]["image_id"]==$posted_data["id2"]&&$r[1]["emotion_id"]!=$posted_data["txt2"]))||(($r[2]["image_id"]==$posted_data["id3"]&&$r[2]["emotion_id"]!=$posted_data["txt3"]))||(($r[3]["image_id"]==$posted_data["id4"]&&$r[3]["emotion_id"]!=$posted_data["txt4"])))
     {  
         $count++;
     }
-     if($r[1]["image_id"]==$posted_data["id2"]&&$r[1]["emotion_id"]!=$posted_data["txt2"])
-    {
-        $count++;
-    }
-     if($r[2]["image_id"]==$posted_data["id3"]&&$r[2]["emotion_id"]!=$posted_data["txt3"])
-    {
-        $count++;
-    }
-     if($r[3]["image_id"]==$posted_data["id4"]&&$r[3]["emotion_id"]!=$posted_data["txt4"])
-    {
-        
-        $count++;
-    }
+    
     $val1 = mysqli_query($conn, 'delete from `active_table`');
     if($count==0)
     {print_r("http://54.186.63.237/emotcha-/bypass.html");}
