c3e803d2ccb954fb0e410b7605c7a631384f9c2e
System Administrator <arjunskmr@gmail.com>
2015-04-20 07:49:25 +0530

    asd

--- signedreg.php
+++ signedreg.php
@@ -6,10 +6,11 @@ $servername = "localhost";
 $username = "root";
 $password = "limboda";
 $length = 30;
+$call= 500;
 
 $appid = substr(str_shuffle("012345fdsfsfsf6789aaaaaabcdefghijklmasdadadnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
 $appsecret = substr(str_shuffle("0123ssdaasdadasdad456789abcdefghasdada2342ijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
-
+$uid = substr(str_shuffle("0123ssdaasda"), 0, 5);
 
 $conn = mysqli_connect ($servername, $username, $password, "emotcha_db");
 
@@ -17,7 +18,7 @@ $conn = mysqli_connect ($servername, $username, $password, "emotcha_db");
   $email = $_POST['email'];
   $password =  $_POST['pass'];
   $cpassword =  $_POST['cpass'];
-  $query = "INSERT INTO user_cred(username,email,password,appid,appsecret) VALUES ('$userName','$email','$password','$appid','$appsecret')";
+  $query = "INSERT INTO user_cred(username,email,password,appid,appsecret,calls) VALUES ('$userName','$email','$password','$appid','$appsecret','$call')";
   $data = mysqli_query($conn, $query);
   if($data)
   {
