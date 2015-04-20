745deeee5f190ceb0e1e2b3d12521d39b6e6580b
System Administrator <arjunskmr@gmail.com>
2015-04-20 08:26:08 +0530

    asd1

--- signedreg.php
+++ signedreg.php
@@ -7,12 +7,13 @@ $password = "limboda";
 $length = 30;
 $appid = substr(str_shuffle("012345fdsfsfsf6789aaaaaabcdefghijklmasdadadnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
 $appsecret = substr(str_shuffle("0123ssdaasdadasdad456789abcdefghasdada2342ijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+$uid = substr(str_shuffle("0123ssdaasdadasdad456789abcdefghasdada2342ijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 7);
 $conn = mysqli_connect ($servername, $username, $password, "emotcha_db");
   $userName = $_POST['uname'];
   $email = $_POST['email'];
   $password =  $_POST['pass'];
   $cpassword =  $_POST['cpass'];
-  $query = "INSERT INTO user_cred(username,email,password,appid,appsecret) VALUES ('$userName','$email','$password','$appid','$appsecret')";
+  $query = "INSERT INTO user_cred(username,email,password,appid,appsecret,uid) VALUES ('$userName','$email','$password','$appid','$appsecret','$uid')";
   $data = mysqli_query($conn, $query);
   if($data)
   {
