9e69956d9908b0a854d21cbc276a8208ee9a5431
System Administrator <arjunskmr@gmail.com>
2015-04-20 08:03:33 +0530

    asd1

--- signedreg.php
+++ signedreg.php
@@ -5,16 +5,14 @@ $servername = "localhost";
 $username = "root";
 $password = "limboda";
 $length = 30;
-$calls=500;
 $appid = substr(str_shuffle("012345fdsfsfsf6789aaaaaabcdefghijklmasdadadnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
 $appsecret = substr(str_shuffle("0123ssdaasdadasdad456789abcdefghasdada2342ijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
-$uid = substr(str_shuffle("0123ssdaasdadasdad456789abcdefghasdada2342ijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 5);
 $conn = mysqli_connect ($servername, $username, $password, "emotcha_db");
   $userName = $_POST['uname'];
   $email = $_POST['email'];
   $password =  $_POST['pass'];
   $cpassword =  $_POST['cpass'];
-  $query = "INSERT INTO user_cred(username,email,password,appid,appsecret,calls,uid) VALUES ('$userName','$email','$password','$appid','$appsecret','$calls','$uid')";
+  $query = "INSERT INTO user_cred(username,email,password,appid,appsecret) VALUES ('$userName','$email','$password','$appid','$appsecret')";
   $data = mysqli_query($conn, $query);
   if($data)
   {
