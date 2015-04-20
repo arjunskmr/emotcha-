efdebe9b24e92bd28ee8b8559a7d902bd1890926
System Administrator <arjunskmr@gmail.com>
2015-04-20 07:57:09 +0530

    asd

--- signedreg.php
+++ signedreg.php
@@ -1,19 +1,13 @@
 <?php
 ini_set('display_errors', 1); 
 error_reporting(E_ALL);
-
 $servername = "localhost";
 $username = "root";
 $password = "limboda";
 $length = 30;
-
-
 $appid = substr(str_shuffle("012345fdsfsfsf6789aaaaaabcdefghijklmasdadadnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
 $appsecret = substr(str_shuffle("0123ssdaasdadasdad456789abcdefghasdada2342ijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
-//$uid = substr(str_shuffle("0123ssdaasda"), 0, 5);
-
 $conn = mysqli_connect ($servername, $username, $password, "emotcha_db");
-
   $userName = $_POST['uname'];
   $email = $_POST['email'];
   $password =  $_POST['pass'];
@@ -24,7 +18,4 @@ $conn = mysqli_connect ($servername, $username, $password, "emotcha_db");
   {
   echo "YOUR REGISTRATION IS COMPLETED...";
   }
-
-
 ?>
-
